68cf7d42605d5518d317965146c160c8aa55a711
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-01-13 16:42:56 +0100

    protected route

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,20 +1,22 @@
 // src/routers/auth.routes.js
 const express = require("express");
 const authController = require("../controllers/authController");
-const authMiddleware = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Authentication routes
+// Auth routes
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
-router.post("/changePassword", authMiddleware, authController.changePassword);
-router.post("/forgotPassword", authController.forgotPassword);
-router.post("/resetPassword", authController.resetPassword);
 router.post("/signout", authController.signout);
 
-// Email verification routes
 router.post("/verifyEmail", authController.verifyEmail);
 router.post("/sendVerificationCode", authController.sendVerificationCode);
+
+// Protected routes
+router.post("/changePassword", authMiddleware, authController.changePassword);
+
+router.post("/forgotPassword", authController.forgotPassword);
+router.post("/resetPassword", authController.resetPassword);
 
 module.exports = router;
